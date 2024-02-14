c8b024c1c1ffbb35104dd4e3d8fe8c166d39dc9b
Wei-04 <weihenghe04@gmail.com>
2024-02-14 19:11:06 +0000

    Commit 1 (initial commit)

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/*! @mainpage Automatic Windshield Wiper Control System
+* @date Wednesday, February 14, 2024
+* @authors Harmon Cleary, Weiheng He
+* @section genDesc General Description
+*
+* This is an implementation of a windshield wiper system to be used in a car.
+* The driver will have the ability to select multiple modes of wipers (elaborate later).
+*
+* @section changelog Changelog
+* | Date | Description |
+* |:----------:|:-----------------------------------------------|
+* | 02/14/2024 | First version of program |
+
+* 
+*
+*/
+
+
+//=====[Libraries]=============================================================
+
+#include "mbed.h" //library imports
+#include "arm_book_lib.h"
+
+//=====[Defines]===============================================================
+
+
+
+//=====[Declaration and initialization of public global objects]===============
+
+
+
+//=====[Declaration and initialization of public global variables]=============
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+
+//=====[Main function, the program entry point after power on or reset]========
+
+
+
+
+//=====[Implementation of global functions]====================================
+
