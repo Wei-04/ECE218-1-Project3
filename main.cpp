f9dd3a4fe71bdcdc8c1c88dd34f19ece4b8ddb84
Wei-04 <weihenghe04@gmail.com>
2024-02-15 17:04:32 +0000

    commit 9 (updated main documentation and readme)

--- main.cpp
+++ main.cpp
@@ -4,7 +4,15 @@
 * @section genDesc General Description
 *
 * This is an implementation of a windshield wiper system to be used in a car.
-* The driver will have the ability to select multiple modes of wipers (elaborate later).
+* When the engine ignition is triggered, requiring the driver to be an occupant,
+* then the engine light LED will turn on and the wiper system can be initiated.
+* The driver will have the ability to select multiple different modes of the wiper
+* system, being high (HI), low (LO), interval (INT), and OFF. HI would spin at 30
+* rpm, LO would spin at 20 rpm, and INT will allow the driver to choose from
+* three different intervals of a rotation every 3, 6, or 8 seconds. The last mode
+* OFF would simply return the wipers to their resting position. In the case of an
+* engine shutdown while the wipers are in use, they shall also go back to resting
+* position with the sole exception of INT which shall remain stationary.
 *
 * @section changelog Changelog
 * | Date | Description |
